53a71ce41393ffb1f0528d4aa122a7864009b1bd
Alexuss951 <alexuss@gmail.com>
2023-11-05 10:54:43 +0100

    test login

--- tf/1-recursive-rg.tf
+++ tf/1-recursive-rg.tf
@@ -1,16 +1,16 @@
-# Variable que indica la cantidad de grupos de recursos que deseas crear
-variable "num_resource_groups" {
-  type    = number
-  default = 1  # Puedes cambiar este valor al número deseado
-}
+# # Variable que indica la cantidad de grupos de recursos que deseas crear
+# variable "num_resource_groups" {
+#   type    = number
+#   default = 1  # Puedes cambiar este valor al número deseado
+# }
 
 
 
 
-# Crear grupos de recursos utilizando un ciclo count
-resource "azurerm_resource_group" "rgs" {
-  count = var.num_resource_groups
+# # Crear grupos de recursos utilizando un ciclo count
+# resource "azurerm_resource_group" "rgs" {
+#   count = var.num_resource_groups
 
-  name     = "rg${count.index + 1}"  # Genera nombres como rg1, rg2, rg3, ...
-  location = "eastus"  # Ubicación recursos
-}
+#   name     = "rg${count.index + 1}"  # Genera nombres como rg1, rg2, rg3, ...
+#   location = "eastus"  # Ubicación recursos
+# }
--- tf/main.tf
+++ tf/main.tf
@@ -1,27 +1,27 @@
 
-# # Crear RG
-# resource "azurerm_resource_group" "rg" {
-#   name     = var.rg_name
-#   location = var.location
-# }
+# Crear RG
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
 
 
-# #Crear AKS cluster
-# resource "azurerm_kubernetes_cluster" "cluster" {
-#   name                = var.aks_cluster_name
-#   location            = var.location
-#   resource_group_name = var.rg_name
-#   dns_prefix          = "akstf"
-#   default_node_pool {
-#     name       = "default"
-#     node_count = var.node_pool_count
-#     vm_size    = var.node_pool_size
-#   }
-#   identity {
-#     type = "SystemAssigned"
-#   }
-#   depends_on = [azurerm_resource_group.rg]
-# }
+#Crear AKS cluster
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = var.aks_cluster_name
+  location            = var.location
+  resource_group_name = var.rg_name
+  dns_prefix          = "akstf"
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_pool_count
+    vm_size    = var.node_pool_size
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+  depends_on = [azurerm_resource_group.rg]
+}
 
 
 
