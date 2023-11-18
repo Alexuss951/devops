e8f67ecf666ed3f0dee7d15d08820afee3c0f144
alx <alexuss@gmail.com>
2023-11-18 15:41:00 +0100

    test deploy

---
+++ tf/0-rg.tf
@@ -0,0 +1,13 @@
+# # Crear grupos de recursos Azure
+# resource "azurerm_resource_group" "RG" {
+
+#   name     = "Prueba"  
+#   location = "eastus"  
+# }
+
+# # Crear grupos de recursos Azure
+# resource "azurerm_resource_group" "RG" {
+
+#   name     = "Prueba"  
+#   location = "eastus"  
+# }
