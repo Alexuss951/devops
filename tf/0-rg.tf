2451b4aa41f7412324d4597c4da34730f4aceeef
alx <alexuss@gmail.com>
2023-11-18 16:04:22 +0100

    test up

--- tf/0-rg.tf
+++ tf/0-rg.tf
@@ -4,10 +4,3 @@ resource "azurerm_resource_group" "RG" {
   name     = "Prueba"  
   location = "eastus"  
 }
-
-# Crear grupos de recursos Azure
-resource "azurerm_resource_group" "RG" {
-
-  name     = "Prueba"  
-  location = "eastus"  
-}
